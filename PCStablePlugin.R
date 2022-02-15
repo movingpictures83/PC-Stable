0cd7eb2f144c488aa88223576d22903c8812963c
Trevor Cickovski <tcickovs@delphi.cs.fiu.edu>
2022-02-15 07:47:51 -0500

    Updated

--- PCStablePlugin.R
+++ PCStablePlugin.R
@@ -1,17 +1,37 @@
-library(pcalg)
-library(igraph)
+#rm(list = ls())
+
+library(bnlearn)
+#setwd("D:\\FIU\\Causal Network Inference in Cancer") # This is input folder, input and output file will be in this directory
+
 
 input <- function(inputfile) {
-  mydata <<- read.csv(inputfile)
+   data_file_name <<- inputfile #input file
+   dff <<- read.csv(data_file_name)
 }
 
+##########################################################################################
+# PC STABLEa
+# Input: asv.transpose.norm.csv
+# Output: Plot
 run <- function() {
-   suffStat <- list(C = cor(mydata), n = nrow(mydata))
-   varNames <- colnames(mydata)
-   results <<- pc(suffStat, indepTest = gaussCItest,
-                labels = varNames, alpha = 0.01)
+print("RUNNING PC STABLE...")
+#### Pc stable Algorithm
+dff <<- dff[,-1]
+dades <- lapply(dff, as.numeric)
+bn_df <- data.frame(dades)
+bn_df <- na.omit(bn_df)
+resGS<<- pc.stable(bn_df, cluster = NULL,whitelist = NULL, blacklist = NULL, test = "zf",
+                  alpha = 0.05, B = NULL, debug = F, undirected = FALSE)
+
+print("DONE.")
 }
 
 output <- function(outputfile) {
-   write.csv(as.matrix(as_adj(graph_from_graphnel(results@graph))), outputfile) 
+print("PLOTTING...")
+####
+plot(resGS)
+saveRDS(resGS, outputfile)
+print("DONE.")
 }
+##########################################################################################
+
